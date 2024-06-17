77838189a3f0b3cf3fe4d17255cbc11dc67a0894
chifuyu08 <sujal.payal08@gmail.com>
2024-06-18 01:55:17 +0530

    navbar scroll

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,12 +4,12 @@ import css from "../design/contact.module.css";
 import { IoMail } from "react-icons/io5";
 import { FaLocationDot } from "react-icons/fa6";
 import { FaPhoneAlt } from "react-icons/fa";
-import { motion } from "framer-motion";
+import { delay, motion } from "framer-motion";
 import { useInView } from 'react-intersection-observer';
 
 
 const Contact = () => {
-    
+
   const variants = {
     initial: {
       y: 500,
@@ -19,7 +19,8 @@ const Contact = () => {
       y: 0,
       opacity: 1,
       transition: {
-        duration: 0.5,
+        delay:0.1,
+        duration: 0.7,
         staggerChildren: 0.1,
       },
     },
@@ -60,7 +61,7 @@ const Contact = () => {
           <FaPhoneAlt className={css.Icon} />
           <div>
             <h2>Phone</h2>
-            <span>+92-8800748596</span>
+            <span>+91-8800748596</span>
           </div>
         </motion.div>
       </motion.div>
@@ -87,7 +88,7 @@ const Contact = () => {
           strokeLinecap="round"
           initial={{ pathLength: 0 }}
           animate={inView && { pathLength: 1 }}
-          transition={{ duration: 3 }}
+          transition={{ duration: 3}}
         />
         <motion.path
           d="M14.207 5.53564C14.207 5.53564 15.197 5.81849 16.6819 7.30341C18.1668 8.78834 18.4497 9.77829 18.4497 9.77829"
@@ -96,7 +97,7 @@ const Contact = () => {
           strokeLinecap="round"
           initial={{ pathLength: 0 }}
           animate={inView && { pathLength: 1 }}
-          transition={{ duration: 3 }}
+          transition={{ duration: 3}}
         />
         <motion.path
           d="M10.0376 5.31617L10.6866 6.4791C11.2723 7.52858 11.0372 8.90532 10.1147 9.8278C10.1147 9.8278 10.1147 9.8278 10.1147 9.8278C10.1146 9.82792 8.99588 10.9468 11.0245 12.9755C13.0525 15.0035 14.1714 13.8861 14.1722 13.8853C14.1722 13.8853 14.1722 13.8853 14.1722 13.8853C15.0947 12.9628 16.4714 12.7277 17.5209 13.3134L18.6838 13.9624C20.2686 14.8468 20.4557 17.0692 19.0628 18.4622C18.2258 19.2992 17.2004 19.9505 16.0669 19.9934C14.1588 20.0658 10.9183 19.5829 7.6677 16.3323C4.41713 13.0817 3.93421 9.84122 4.00655 7.93309C4.04952 6.7996 4.7008 5.77423 5.53781 4.93723C6.93076 3.54428 9.15317 3.73144 10.0376 5.31617Z"
@@ -105,7 +106,7 @@ const Contact = () => {
           strokeLinecap="round"
           initial={{ pathLength: 0 }}
           animate={inView && { pathLength: 1 }}
-          transition={{ duration: 3 }}
+          transition={{ duration: 3}}
         />
       </svg>
     </motion.div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,58 @@ import { FaHome } from "react-icons/fa";
 import { AiOutlineFundProjectionScreen } from "react-icons/ai";
 import { ImBooks } from "react-icons/im";
 import { TbMessage } from "react-icons/tb";
-const Navbar=()=>{
-    return(
-        <nav className="navbar">
-            <a href="#Homeid"><FaHome/></a>
-            <a href="#Projectid"><AiOutlineFundProjectionScreen/></a>
-            <a href="#Skillsid"><ImBooks/></a>
-            <a href="#Contactid"><TbMessage/></a>
-        </nav>
-    )
-}
+import { Link } from "react-scroll";
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <Link
+        activeClass="active"
+        to="Homeid"
+        spy={true}
+        smooth={true}
+        offset={-70} // Adjust offset for potential navbar height
+        duration={500} // Customize scrolling duration (milliseconds)
+      >
+        <FaHome />
+      </Link>
+      <Link
+        activeClass="active"
+        to="Projectid"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+      >
+        <AiOutlineFundProjectionScreen />
+      </Link>
+      <Link
+        activeClass="active"
+        to="Skillsid"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+      >
+        <ImBooks />
+      </Link>
+      <Link
+        activeClass="active"
+        to="Contactid"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+      >
+        <TbMessage />
+      </Link>
+      {/* ... similar links for other sections */}
+    </nav>
+    // <nav className="navbar">
+    //     <a href="#Homeid"><FaHome/></a>
+    //     <a href="#Projectid"><AiOutlineFundProjectionScreen/></a>
+    //     <a href="#Skillsid"><ImBooks/></a>
+    //     <a href="#Contactid"><TbMessage/></a>
+    // </nav>
+  );
+};
 export default Navbar;
